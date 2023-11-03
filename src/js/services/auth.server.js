d3f027ebb6d49f8b9019956028a313d09a628c89
Pavel-VV <vahonin7@mail.ru>
2023-11-03 23:12:27 +0300

    update validation userData on email

--- src/js/services/auth.server.js
+++ src/js/services/auth.server.js
@@ -37,23 +37,20 @@ export function regServer(userData) {
         validationKeys.forEach(keyValid => { // перебираем требуемые ключи сервера
             const resoult = userKeys.some(keyUser => { // сравниваем каждый ключ пришедшего юзера с ключем требуемым сервером
                 return keyUser === keyValid;
-                
             })
             if (!resoult) return resoultValidationUserKey = false; // если хотя бы одного ключа нет, то в переменную resoultValidationUserKey записываем false
             resoultValidationUserKey = true;
         });
         if(resoultValidationUserKey) {
-            const resoultNewUser = addNewUser.some(user => { //перебираю всех сохраненных пользователей на сервере сохраненные в json формате с новым пользователем, предварительно переведя его в json формат
-                console.log(addNewUser, user)
-                return JSON.stringify(user) === JSON.stringify(userData);
-                
-                
+            const resoultNewUser = addNewUser.some(user => { //перебираю всех сохраненных пользователей на сервере
+                console.log(user.email, userData.email)
+                if(user.email === userData.email)return true; // проверяю регистрировался ли этот пользователь на сервере
             });
-            console.log(resoultNewUser)
-            if(resoultNewUser) return // если нашлось совпадение среди сохраненных пользователей, прекращаем дальнейший код, если совпадений небыло, то переводим нового пользователя в json и добавляем в массив пользователей
+            // console.log(resoultNewUser);
+            if(resoultNewUser) return reject(false) // если нашлось совпадение среди сохраненных пользователей, прекращаем дальнейший код, если совпадений небыло, то добавляем в массив пользователей
             
-            addNewUser.push(JSON.parse(JSON.stringify(userData))); // записываю объекты в json формате в массив для того чтоб можно было сравнивать, не добавился ли точно такой же пользователь
-            console.log(addNewUser)
+            addNewUser.push(JSON.parse(JSON.stringify(userData))); // записываю объекты в json формате в массив для того чтоб можно было сравнивать, не добавился ли точно такой же пользователь(нужно парсить и распарсить, так как объект копируется по ссылке, и когда приходит новый объект, он заменяется в массиве приходящим, так как по ссылке)
+            console.log(addNewUser);
             resolve({
                 error: false,
                 auth: true,
